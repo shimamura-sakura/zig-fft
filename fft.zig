fcb697698ebc1113fedfbfcd7d428bb6ebd059b4
Shimamura Sakura <shimamura_sakura@pm.me>
2023-12-18 16:44:00 +0800

    update to zig 0.12

--- fft.zig
+++ fft.zig
@@ -12,14 +12,13 @@ pub fn ifft(comptime F: type, real: []F, imag: []F) FFTError!void {
     if (@popCount(real.len) != 1) return FFTError.SizeNotPow2;
     for (imag) |*v| v.* = -v.*;
     fft(F, real, imag) catch unreachable;
-    for (real) |*v| v.* /= @intToFloat(F, real.len);
-    for (imag) |*v| v.* /= @intToFloat(F, imag.len) * -1.0;
+    for (real) |*v| v.* /= @as(F, @floatFromInt(real.len));
+    for (imag) |*v| v.* /= @as(F, @floatFromInt(imag.len)) * -1.0;
 }
 fn shuffle(comptime F: type, real: []F, imag: []F) void {
     const shrAmount = @bitSizeOf(usize) - @ctz(real.len);
-    const shrAmtType = std.meta.Int(.unsigned, @ctz(@as(u8, @bitSizeOf(usize))));
-    for (real) |_, i| {
-        const j = @bitReverse(i) >> @intCast(shrAmtType, shrAmount);
+    for (real, 0..) |_, i| {
+        const j = @bitReverse(i) >> @intCast(shrAmount);
         if (i >= j) continue;
         std.mem.swap(F, &real[i], &real[j]);
         std.mem.swap(F, &imag[i], &imag[j]);
@@ -30,9 +29,11 @@ fn compute(comptime F: type, real: []F, imag: []F) void {
     while (step < real.len) : (step <<= 1) {
         var group: usize = 0;
         const jump = step << 1;
+        const stp: F = @floatFromInt(step);
         while (group < step) : (group += 1) {
-            const t_re = @cos(-PI * @intToFloat(F, group) / @intToFloat(F, step));
-            const t_im = @sin(-PI * @intToFloat(F, group) / @intToFloat(F, step));
+            const rads: F = -PI * @as(F, @floatFromInt(group)) / stp;
+            const t_re = @cos(rads);
+            const t_im = @sin(rads);
             var pair = group;
             while (pair < real.len) : (pair += jump) {
                 const match = pair + step;
@@ -45,4 +46,24 @@ fn compute(comptime F: type, real: []F, imag: []F) void {
             }
         }
     }
+}
+
+test "main" {
+    var real = [8]f32{ 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0 };
+    var imag = [8]f32{ -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 };
+    std.debug.print("IN:\n", .{});
+    std.debug.print("real: {d:7.3}\n", .{real});
+    std.debug.print("imag: {d:7.3}\n", .{imag});
+
+    try fft(f32, &real, &imag);
+
+    std.debug.print("FFT:\n", .{});
+    std.debug.print("real: {d:7.3}\n", .{real});
+    std.debug.print("imag: {d:7.3}\n", .{imag});
+
+    try ifft(f32, &real, &imag);
+
+    std.debug.print("IFFT:\n", .{});
+    std.debug.print("real: {d:7.3}\n", .{real});
+    std.debug.print("imag: {d:7.3}\n", .{imag});
 }
